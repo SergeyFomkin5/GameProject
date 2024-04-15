768a226b03ed9af8dedfac2762e3202ad164eee6
tttllliiinnn <qirillsssss18@gmail.com>
2024-04-15 17:19:05 +0300

    New Elevator Script
    - Animations Elevator
    + Scripts Elevator

---
+++ Assets/ElevatorSystem.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ElevatorSystem : MonoBehaviour
+{
+    [SerializeField] private GameObject Player;
+    [SerializeField] private WaitForSeconds PlayerControllerBoolTimer = new WaitForSeconds(0.1f);
+    [SerializeField] private Transform Elevator;
+    [SerializeField] private Transform TargetPositionUp;
+    [SerializeField] private Transform TargetPositionDown;
+    [SerializeField] private Transform TargetPositionPlayer;
+    [SerializeField] private bool MoveElevator = true;
+     public bool ActiveTargetPlayer=false;
+    void Update()
+    {
+        if (ActiveTargetPlayer == true)
+        {
+            Player.transform.position = new Vector3(Player.transform.position.x, TargetPositionPlayer.position.y, Player.transform.position.z);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                PlayerControllerBool();
+                if (MoveElevator == true)
+                {
+                    MoveElevator = false;
+                }
+                else
+                {
+                    MoveElevator = true;
+                }
+            }
+        }
+        if (Input.GetKeyUp(KeyCode.E))
+        {
+            MoveElevator = true;
+        }
+        if (MoveElevator == true)
+        {
+            
+            Elevator.position = TargetPositionUp.position;
+            
+        }
+        if (MoveElevator == false)
+        { 
+            Elevator.position = TargetPositionDown.position;
+
+        }
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("1");
+            ActiveTargetPlayer = true;
+        }
+    }
+    IEnumerator PlayerControllerBool()
+    {
+        Player.GetComponent<PlayerController>().enabled = false;
+        yield return PlayerControllerBoolTimer;
+        Player.GetComponent<PlayerController>().enabled = true;
+    }
+}
