6ecc59bbcab3ca9388cb995a47541cd037d54cce
El1GF <159153855+El1GF@users.noreply.github.com>
2024-04-17 18:09:26 +0300

    Sorting

---
+++ Assets/Models and skirpts/Scripts/PlayerController/PlayerController.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 1.5f;
+
+    [SerializeField] private Transform head;
+
+    [SerializeField] private float sensitivity = 5f; // чувствительность мыши
+    [SerializeField] private float headMinY = -40f; // ограничение угла для головы
+    [SerializeField] private float headMaxY = 40f;
+
+    [SerializeField] private KeyCode jumpButton = KeyCode.Space; // клавиша для прыжка
+    [SerializeField] private float jumpForce = 10; // сила прыжка
+    [SerializeField] private float jumpDistance = 1.2f; // расстояние от центра объекта, до поверхности
+
+    [SerializeField] private  Vector3 direction;
+    [SerializeField] private  float h, v;
+    [SerializeField] private  int layerMask;
+    [SerializeField] private  Rigidbody body;
+    [SerializeField] private  float rotationY;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody>();
+        body.freezeRotation = true;
+        layerMask = 3;
+    }
+
+    void FixedUpdate()
+    {
+        body.AddForce(direction * speed, ForceMode.VelocityChange);
+
+        // Ограничение скорости, иначе объект будет постоянно ускоряться
+        if (Mathf.Abs(body.velocity.x) > speed)
+        {
+            body.velocity = new Vector3(Mathf.Sign(body.velocity.x) * speed, body.velocity.y, body.velocity.z);
+        }
+        if (Mathf.Abs(body.velocity.z) > speed)
+        {
+            body.velocity = new Vector3(body.velocity.x, body.velocity.y, Mathf.Sign(body.velocity.z) * speed);
+        }
+    }
+
+    bool GetJump() // проверяем, есть ли коллайдер под ногами
+    {
+        RaycastHit hit;
+        Ray ray = new Ray(transform.position, Vector3.down);
+        if (Physics.Raycast(ray, out hit, jumpDistance, layerMask))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical");
+
+        // управление головой (камерой)
+        float rotationX = head.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivity;
+        rotationY += Input.GetAxis("Mouse Y") * sensitivity;
+        rotationY = Mathf.Clamp(rotationY, headMinY, headMaxY);
+        head.localEulerAngles = new Vector3(-rotationY, rotationX, 0);
+
+        // вектор направления движения
+        direction = new Vector3(h, 0, v);
+        direction = head.TransformDirection(direction);
+        direction = new Vector3(direction.x, 0, direction.z);
+
+        if (Input.GetKeyDown(jumpButton) && GetJump())
+        {
+            body.velocity = new Vector2(0, jumpForce);
+        }
+    }
+
+    void OnDrawGizmosSelected() // подсветка, для визуальной настройки jumpDistance
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(transform.position, Vector3.down * jumpDistance);
+    }
+}
---
+++ Assets/Models and skirpts/Scripts/Triggers/TriggerShuttleBack.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TriggerShuttleBack : MonoBehaviour
+{
+    [SerializeField] private GameObject PlayerPosition;
+    [SerializeField] private Transform TargetPlayerPosition;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            if (Input.GetKey(KeyCode.E))
+            {
+                PlayerPosition.transform.position = TargetPlayerPosition.position;
+            }
+        }
+    }
+
+}
