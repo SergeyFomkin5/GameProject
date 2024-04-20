d319881302671bacccae55a21156b0fc910d7433
Сергей Фомкин <144447986+SergeyFomkin5@users.noreply.github.com>
2024-04-20 15:16:57 +0300

    Create Currency Display

---
+++ Assets/Scripts/CurrencySystem/CurrencyManager.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using TMPro;
+
+public class CurrencyManager : MonoBehaviour
+{
+    public static float Currency;
+    public TextMeshProUGUI CurrencyTXT;
+    public Text CurrencyTXTtest;
+
+    private void Start()
+    {
+        CurrencyTXT.text = "$: " + Currency.ToString();
+        CurrencyTXTtest.text = "$: " + Currency.ToString();
+    }
+
+    //void Update()
+    //{
+    //    CurrencyTXT.text = "$:" + Currency.ToString();
+    //}
+
+    public void AddCredits()
+    {
+
+        Currency += 10;
+
+        Debug.Log("+");
+
+        CurrencyTXT.text = "$:" + Currency.ToString();
+
+    }
+
+    public void minusCredits()
+    {
+
+        Currency -= 10;
+
+        Debug.Log("-");
+
+        CurrencyTXT.text = "$:" + Currency.ToString();
+
+    }
+}
