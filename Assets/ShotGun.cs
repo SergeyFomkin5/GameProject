2ff075ca8cd5f4e5725edfb05cb306b21c20877f
tttllliiinnn <qirillsssss18@gmail.com>
2024-04-27 19:58:04 +0300

    new mechanics ShotGun

---
+++ Assets/ShotGun.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class ShotGun : MonoBehaviour
+{
+    [SerializeField] private Transform Cub;
+    [SerializeField] private Transform TargetMin;
+    [SerializeField] private Transform TargetMax;
+    [SerializeField] private Transform EndGan;
+    [SerializeField] private float NextFire;
+    RaycastHit hit;
+    Ray ray;
+    private void Update()
+    {
+        if (Input.GetKey(KeyCode.Mouse1) && Time.time > NextFire) 
+        {
+            NextFire = Time.time + .0f; 
+            Shoot(); 
+        }
+    }
+    public void Shoot()
+    {
+        for (int i = 0; i < 10; i++)
+        {
+            var direction =TargetMax.position - TargetMin.position+ Vector3.forward* Random.Range(-3, 3)+ Vector3.up * Random.Range(-3, 3);
+            ray = new Ray(TargetMin.position, direction);
+            Physics.Raycast(ray, out hit);
+            Debug.DrawRay(EndGan.position, direction, Color.yellow);
+            if (hit.collider.gameObject)
+            {
+                Instantiate(Cub, hit.point, Quaternion.identity);
+            }
+        }   
+        
+    }
+}
