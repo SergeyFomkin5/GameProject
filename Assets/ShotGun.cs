2160268f7f0c76a7332ff7680f4f854517c18752
tttllliiinnn <qirillsssss18@gmail.com>
2024-04-27 22:09:36 +0300

    добавил новую механику с игроком пока что в разработке

--- Assets/ShotGun.cs
+++ Assets/ShotGun.cs
@@ -2,34 +2,55 @@
 
 public class ShotGun : MonoBehaviour
 {
-    [SerializeField] private Transform Cub;
+    [SerializeField] private GameObject[] Players=new GameObject[2];
+    [SerializeField] private GameObject Cub;
     [SerializeField] private Transform TargetMin;
     [SerializeField] private Transform TargetMax;
     [SerializeField] private Transform EndGan;
+    [SerializeField] private bool ActiveClone = false, OneOrTwo=true; 
     [SerializeField] private float NextFire;
     RaycastHit hit;
     Ray ray;
     private void Update()
     {
-        if (Input.GetKey(KeyCode.Mouse1) && Time.time > NextFire) 
+        if (ActiveClone==false&&Input.GetKey(KeyCode.Mouse1) && Time.time > NextFire) 
         {
             NextFire = Time.time + .0f; 
-            Shoot(); 
+            Shoot();
+            ActiveClone = true;
         }
+        OneOrTwoPlayer();
     }
     public void Shoot()
     {
-        for (int i = 0; i < 10; i++)
+        for (int i = 0; i < 13; i++)
         {
-            var direction =TargetMax.position - TargetMin.position+ Vector3.forward* Random.Range(-3, 3)+ Vector3.up * Random.Range(-3, 3);
+            var direction = TargetMax.position - TargetMin.position + Vector3.forward * Random.Range(-3, 3) + Vector3.up * Random.Range(-3, 3);
             ray = new Ray(TargetMin.position, direction);
             Physics.Raycast(ray, out hit);
             Debug.DrawRay(EndGan.position, direction, Color.yellow);
             if (hit.collider.gameObject)
             {
-                Instantiate(Cub, hit.point, Quaternion.identity);
             }
-        }   
         
+        }
+    }
+    void PlayerClone()
+    {
+        Instantiate(Cub, hit.point, Quaternion.identity);
+        Players[1] = Cub; 
+    }
+    void OneOrTwoPlayer()
+    {
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            Players[0].SetActive(false);
+            Players[1].SetActive(true);
+        }
+        else
+        {
+            Players[0].SetActive(true);
+            Players[1].SetActive(false);
+        }
     }
 }
