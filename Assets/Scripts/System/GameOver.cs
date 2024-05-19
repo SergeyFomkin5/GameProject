5f6c2d8724ca032fa74597f8b2bb1e21fd37da79
Сергей Фомкин <144447986+SergeyFomkin5@users.noreply.github.com>
2024-05-19 22:08:24 +0300

    Make GameOverScreen and Timer

---
+++ Assets/Scripts/System/GameOver.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    Timer timer;
+    public GameObject GameOverScreen;
+
+
+    private void Start()
+    {
+        timer = GetComponent<Timer>();
+    }
+
+    void Update()
+    {
+        if (timer.timeRemaining <= 0)
+        {
+            Debug.Log("something happend");
+            Time.timeScale = 0f;
+            GameOverScreen.SetActive(true);
+        }
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+
+    public void ExitMenu()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/System/Timer.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float timeRemaining;
+    public bool timerIsRunning = false;
+    public TMP_Text timeText;
+    public GameObject GameOverScreen;
+
+
+    private void Start()
+    {
+        timerIsRunning = true;
+    }
+
+    void Update()
+    {
+        if (timeRemaining <= 0)
+        {
+            Debug.Log("something happend");
+            Time.timeScale = 0f;
+            GameOverScreen.SetActive(true);
+        }
+
+        if (timerIsRunning)
+        {
+            if (timeRemaining > 0)
+            {
+                timeRemaining -= Time.deltaTime;
+                DisplayTime(timeRemaining);
+            }
+            else
+            {
+                timeRemaining = 0;
+                timerIsRunning = false;
+            }
+        }
+    }
+
+    public void DisplayTime(float timeToDisplay)
+    {
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+
+        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+}
